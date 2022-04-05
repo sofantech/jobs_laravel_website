7c579f11214af1c1348535bbd3cfbef1f2e13068
sofantech <85455568+sofantech@users.noreply.github.com>
2022-04-05 03:45:31 +0300

    create database and tables

--- database/migrations/2022_04_04_233918_create_categories_table.php
+++ database/migrations/2022_04_04_233918_create_categories_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateJobsTable extends Migration
+class CreateCategoriesTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,15 +13,10 @@ class CreateJobsTable extends Migration
      */
     public function up()
     {
-        Schema::create('jobs', function (Blueprint $table) {
+        Schema::create('categories', function (Blueprint $table) {
             $table->id();
-            $table->string("name");
-            $table->string("details");
-            $table->string("job_type");
-            $table->string("job_field");
-            $table->string("Experience");
-            $table->string("salary");
-            $table->string("job_date");
+            $table->text("name");
+            $table->text("img");
             $table->timestamps();
         });
     }
@@ -33,6 +28,6 @@ class CreateJobsTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('jobs');
+        Schema::dropIfExists('categories');
     }
 }
